0bd898c6af74146274d4eceb8ad03c0de83233af
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-01-01 14:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const time=document.querySelector("#time");
+window.addEventListener("DOMContentLoaded",(e)=>{
+	e.preventDefault();
+	const now=new Date();
+	time.innerHTML=now.toLocaleString();
+})
